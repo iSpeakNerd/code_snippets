677c1d2dff8e103456bb19d39c0e78dba6b5639b
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2025-05-26 12:31:23 -0700

    04

--- Python/Flask_Blog/04-Database/flaskblog.py
+++ Python/Flask_Blog/04-Database/flaskblog.py
@@ -1,32 +1,57 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from pathlib import Path
+
+import os
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+
+SB_PASSWORD = os.environ["SUPABASE_PASSWORD"]
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+# Supabase stateless ipv4 - preferred
+# app.config["SQLALCHEMY_DATABASE_URI"] = (f"postgresql://postgres.ekiwycazuqpglzqcmfly:{SB_PASSWORD}@aws-0-us-west-1.pooler.supabase.com:6543/postgres")
+# Supabase stateful ipv4 - fallback?
+# app.config["SQLALCHEMY_DATABASE_URI"] = (f"postgresql://postgres.ekiwycazuqpglzqcmfly:{SB_PASSWORD}@aws-0-us-west-1.pooler.supabase.com:5432/postgres")
 db = SQLAlchemy(app)
+app.app_context().push()
 
 
-class User(db.Model):
+def make():
+    # with app.app_context():
+    db.create_all()
+    pass
+
+
+# declarative sql table
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    # image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship("Posts", backref="author", lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}')"
+        # return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.Model):
+class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(
+        db.DateTime, nullable=False, default=lambda: datetime.now(timezone.utc)
+    )
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
@@ -34,51 +59,51 @@ class Post(db.Model):
 
 posts = [
     {
-        'author': 'Corey Schafer',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'April 20, 2018'
+        "author": "Corey Schafer",
+        "title": "Blog Post 1",
+        "content": "First post content",
+        "date_posted": "April 20, 2018",
     },
     {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'April 21, 2018'
-    }
+        "author": "Jane Doe",
+        "title": "Blog Post 2",
+        "content": "Second post content",
+        "date_posted": "April 21, 2018",
+    },
 ]
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts=posts)
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template("about.html", title="About")
 
 
-@app.route("/register", methods=['GET', 'POST'])
+@app.route("/register", methods=["GET", "POST"])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
 
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        if form.email.data == "admin@blog.com" and form.password.data == "password":
+            flash("You have been logged in!", "success")
+            return redirect(url_for("home"))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
-    return render_template('login.html', title='Login', form=form)
+            flash("Login Unsuccessful. Please check username and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
