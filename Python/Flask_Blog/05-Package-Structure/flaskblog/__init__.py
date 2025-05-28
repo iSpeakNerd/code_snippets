2c70a5e2e9c04966dafd03a2fbc5f9f6af5c1a88
iSpeakNerd <96841384+iSpeakNerd@users.noreply.github.com>
2025-05-28 11:02:38 -0700

    05

--- Python/Flask_Blog/05-Package-Structure/flaskblog/__init__.py
+++ Python/Flask_Blog/05-Package-Structure/flaskblog/__init__.py
@@ -1,9 +1,25 @@
 from flask import Flask
+from pathlib import Path
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+import os
+from dotenv import load_dotenv
+
+env_path = Path(__file__).resolve().parent / ".env"
+load_dotenv(dotenv_path=env_path)
+SB_PASSWORD = os.environ["SUPABASE_PASSWORD"]
+
+app.config["SECRET_KEY"] = "5791628bb0b13ce0c676dfde280ba245"
+
+# site.db inside ./instance/
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
+
+# Supabase stateless ipv4 - preferred
+# app.config["SQLALCHEMY_DATABASE_URI"] = (f"postgresql://postgres.ekiwycazuqpglzqcmfly:{SB_PASSWORD}@aws-0-us-west-1.pooler.supabase.com:6543/postgres")
+# Supabase stateful ipv4 - fallback?
+# app.config["SQLALCHEMY_DATABASE_URI"] = (f"postgresql://postgres.ekiwycazuqpglzqcmfly:{SB_PASSWORD}@aws-0-us-west-1.pooler.supabase.com:5432/postgres")
 db = SQLAlchemy(app)
+app.app_context().push()
 
 from flaskblog import routes
--- Python/Flask_Blog/05-Package-Structure/flaskblog/models.py
+++ Python/Flask_Blog/05-Package-Structure/flaskblog/models.py
@@ -1,25 +1,30 @@
-from datetime import datetime
+from datetime import datetime, timezone
+
+# from __init__.py
 from flaskblog import db
 
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    image_file = db.Column(db.String(20), nullable=False, default="default.jpg")
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
+    posts = db.relationship("Posts", backref="author", lazy=True)
 
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+        return f"User('{self.username}', '{self.email}')"
+        # return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
-class Post(db.Model):
+class Posts(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    date_posted = db.Column(
+        db.DateTime, nullable=False, default=datetime.now(timezone.utc)
+    )
     content = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
